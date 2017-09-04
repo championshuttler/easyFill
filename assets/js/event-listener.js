fff6d40c9b9cdf41f065c4848c8e44f959faba68
championshuttler <shivams2799@gmail.com>
2017-09-05 00:43:41 +0530

    updateUI

--- assets/js/event-listener.js
+++ assets/js/event-listener.js
@@ -2,3 +2,19 @@
 * @Author: championshuttler
 * @Date:   2016-09-05  00:39:44
 */
+
+var list_keys = [ 'personal', 'email', 'github', 'linkedin', 'twitter', 'facebook', 'personal', 'email', 'mobile'];
+
+// updateUI show saved links when popup page is opened by clicking on browser icon
+function updateUI(){
+	for(var i in list_keys){
+		var key = list_keys[i];
+		var link = localStorage.getItem(key);
+		if(typeof link == "string"){
+			document.getElementById(key).value = link;
+		}
+		else{
+			document.getElementById(key).value = "";
+		}
+	}
+}
