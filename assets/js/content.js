e34dfcfb8d6168098eada9249d1a983a6fbcddf9
championshuttler <shivams2799@gmail.com>
2017-09-04 17:05:01 +0530

    textInput

--- assets/js/content.js
+++ assets/js/content.js
@@ -2,3 +2,33 @@
 * @Author: championshuttler
 * @Date:   2017-09-04 04:57:34
 */
+
+var element;
+
+ document.addEventListener("contextmenu", function(e){
+   element = e.target ;
+ });
+
+
+ var textAcceptingInputTypes = [
+ 	"text",
+ 	"url",
+  "number",
+ 	"search",
+ 	"tel",
+ 	"password"
+ ];
+
+
+ var forbiddenTextAcceptingInputTypes = [
+	"number",
+	"email",
+	"range",
+	"date",
+	"month",
+	"week",
+	"time",
+	"datetime",
+	"datetime-local",
+	"color"
+];
