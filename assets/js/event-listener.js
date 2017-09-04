33e8a8c432ade1e7e07b8557a274808045283b39
championshuttler <shivams2799@gmail.com>
2017-09-05 01:08:34 +0530

    final function

--- assets/js/event-listener.js
+++ assets/js/event-listener.js
@@ -1,7 +1,7 @@
 /*
 * @Author: championshuttler
 * @Date:   2017-09-05  00:39:44
- @LastUpdated : 2017-09-05 00:45:45        coding with weedy eyes :P
+ @LastUpdated : 2017-09-05 01:08:25        coding with weedy eyes :P
 */
 
 var list_keys = [ 'personal', 'email', 'github', 'linkedin', 'twitter', 'facebook', 'mobile'];
@@ -25,3 +25,32 @@ function animate_text(id){
 	text_div.show();
 	text_div.fadeOut(2000);
 }
+
+
+
+$(document).on('ready', function(){
+
+	$("#save_links").on('click', function(e){
+		document.getElementById("save_links").innerHTML = "Saving..";
+		for(var i in list_keys){
+			var key = list_keys[i];
+			var newValue = document.getElementById(key).value;
+			localStorage.setItem(key, newValue);
+		}
+		// trigger to show the saved links
+		updateUI();
+		animate_text("#success_text");
+
+		document.getElementById("save_links").innerHTML = "Save links";
+	});
+
+	$("#remove").on("click", function(){
+		localStorage.clear();
+		updateUI();
+
+		animate_text("#remove_text")
+	});
+
+
+
+});
