b9b2af18ef8098a9cd8bea3e0ac092aa6bc6e235
championshuttler <shivams2799@gmail.com>
2017-09-05 11:11:42 +0530

    f@$king error

--- assets/js/background.js
+++ assets/js/background.js
@@ -6,7 +6,7 @@
 
 
 var parent = browser.contextMenus.create({
-	"title": "Paste links here",
+	"title": "Paste  link here",
 	"contexts": ["editable"],
 	"onclick": pasteLink
 });
@@ -61,6 +61,7 @@ var facebook = browser.contextMenus.create({
 var mobile = browser.contextMenus.create({
 	"title": "Mobile Number",
 	"contexts": ["editable"],
+	"parentId" : parent,
 	"onclick": pasteLink
 });
 
